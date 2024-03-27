0e82b65e2834d9d14d6a5cd4205826a9cfadf87b
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-03-27 15:50:31 +0200

    BlackJack logic && styling

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+// Initial player data
+let player = {
+  name: "User",
+  chips: 50,
+};
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+// Initialization of variables
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isBust = true;
+let message = "";
+let gameState = document.getElementById("gameState");
+let hand = document.getElementById("hand");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
 
-let firstCard = 6
-let secondCard = 9
+// Displays Player name && and chips
+playerEl.textContent = player.name + "'s Chips: " + player.chips;
 
-let sum = firstCard + secondCard
+// Generates a random number between 13 => 2 to 11
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
 
-console.log(sum)
+// Initialization of game
+function startGame() {
+  isBust = false;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+// Displays Cards && checks if you won or not
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  hand.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isBust = true;
+  }
+  gameState.textContent = message;
+}
+
+// Checks if bust && allows you to get another
+function newCard() {
+  if (isBust === false && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
+}
+
+// newCard function does brake (Not just here but also on Scrimba)
